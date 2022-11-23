875a8a7eac4c1b5fe1f50bf7db77a6262b7d66a5
dtpvy <dtpvy0202@gmail.com>
2022-11-24 01:59:12 +0700

    fix(layouts): fix home and main layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { layout: 'login_form' });
+  res.render('index', { layout: 'home' });
 });
 
 module.exports = router;
