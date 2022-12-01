6a2b1f847f28a8675207a63f68b5868da6c31c0f
qmck3 <quangfreshmail123@gmail.com>
2022-12-02 00:28:05 +0700

    code web tĩnh các trang booking

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,7 +1,11 @@
 const controller = {};
 
 controller.default = (req, res) => {
-  res.render('booking/step1', { layout: 'booking' });
+  res.render('./booking/booked_ticket', { layout: 'main' });
+};
+
+controller.showStep2 = (req, res) => {
+  res.render('booking/step_2', { layout: 'booking' });
 };
 
 module.exports = controller;
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -7,4 +7,8 @@ router.get('/', function (req, res, next) {
   controller.default(req, res);
 });
 
+router.get('/step_2', function (req, res, next) {
+  controller.showStep2(req, res);
+});
+
 module.exports = router;
