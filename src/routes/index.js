3613d4c6f074b0a53af5244df2f21e11824aee1f
MichaMichaHii <minhchanhwork@gmail.com>
2022-12-02 18:50:52 +0700

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { layout: 'rating' });
+  res.render('index', { layout: 'search' });
 });
 
 module.exports = router;
