7866835cc3286a9f3c5d6985a68a77ba47f83671
qmck3 <quangfreshmail123@gmail.com>
2023-01-02 01:11:10 +0700

    thêm edit order page, update lại các trang booking

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,24 +1,6 @@
 const controller = {};
 const models = require('../models');
 const sequelize = require('sequelize');
-controller.default = (req, res) => {
-  const busSeat = [
-    { booked: 1, seatNum: 1 },
-    { booked: 1, seatNum: 2 },
-    { booked: 0, seatNum: 3 },
-    { booked: 0, seatNum: 4 },
-    { booked: 1, seatNum: 5 },
-    { booked: 0, seatNum: 6 },
-    { booked: 0, seatNum: 7 },
-    { booked: 1, seatNum: 8 },
-    { booked: 0, seatNum: 9 },
-    { booked: 1, seatNum: 10 },
-  ];
-  res.render('booking/step_1', {
-    layout: 'main',
-    busSeat: busSeat,
-  });
-};
 
 controller.showBookingPage = async (req, res) => {
   let id = req.params.id;
@@ -43,16 +25,6 @@ controller.showBookingPage = async (req, res) => {
     include: [
       {
         model: models.Garage,
-        //     include: [
-        //       {
-        //         model: models.Station,
-        //         required: true,
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     model: models.Route,
-        //     required: true,
       },
     ],
   });
@@ -89,11 +61,13 @@ controller.showBookingPage = async (req, res) => {
 
   models.Order.create({
     routeId: res.locals.route.id,
+    userId: user.id,
   });
   res.locals.order = await models.Order.findOne({
     where: {
       routeId: res.locals.route.id,
     },
+    order: [['createdAt', 'DESC']],
     include: [
       {
         model: models.Route,
@@ -101,15 +75,112 @@ controller.showBookingPage = async (req, res) => {
       },
     ],
   });
+
   res.render('booking/step_1', {
     layout: 'main',
-    busSeat: busSeat,
-    user: user,
-    day: day,
+    busSeat,
+    user,
+    day,
+    seatChosen: 0,
   });
 };
 
-controller.showBookedTicket = (req, res) => {
+controller.editOrderPage = async (req, res) => {
+  let id = req.params.id;
+  const user = await models.User.findByPk(req.userId);
+
+  const busSeat = [
+    { booked: 1, seatNum: 1 },
+    { booked: 1, seatNum: 2 },
+    { booked: 0, seatNum: 3 },
+    { booked: 0, seatNum: 4 },
+    { booked: 1, seatNum: 5 },
+    { booked: 0, seatNum: 6 },
+    { booked: 0, seatNum: 7 },
+    { booked: 1, seatNum: 8 },
+    { booked: 0, seatNum: 9 },
+    { booked: 1, seatNum: 10 },
+  ];
+
+  res.locals.order = await models.Order.findOne({
+    where: {
+      id: id,
+    },
+    include: [
+      {
+        model: models.Route,
+        required: true,
+        include: [
+          {
+            model: models.Coach,
+            required: true,
+          },
+        ],
+      },
+    ],
+  });
+
+  // res.locals.seat = await models.Seat.findOne({
+  //   where: {
+  //     coachId: id,
+  //   },
+  //   include: [
+  //     {
+  //       model: models.Coach,
+  //       required: true,
+  //     },
+  //   ],
+  // });
+
+  res.render('booking/edit_order', {
+    layout: 'main',
+    busSeat,
+    user,
+    seatChosen: 0,
+  });
+};
+
+controller.showBookedTicket = async (req, res) => {
+  const id = req.params.id;
+  const user = await models.User.findByPk(req.userId);
+
+  res.locals.order = await models.Order.findOne({
+    where: {
+      id: id,
+    },
+    include: [
+      {
+        model: models.Route,
+        include: [
+          {
+            model: models.Station,
+            as: 'startStation',
+            required: true,
+          },
+          {
+            model: models.Station,
+            as: 'endStation',
+            required: true,
+          },
+          {
+            model: models.Coach,
+            required: true,
+            // include: [
+            //   {
+            //     model: models.Garage,
+            //     required: true,
+            //   },
+            // ],
+          },
+        ],
+      },
+      {
+        model: models.User,
+        required: true,
+      },
+    ],
+  });
+
   res.render('booking/booked_ticket', { layout: 'main' });
 };
 
--- src/routes/booking.js
+++ src/routes/booking.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/bookingController');
+const { verifyAuth } = require('../middlewares/verifyAuth');
 
 /* GET booking. */
 router.get('/', function (req, res, next) {
   controller.default(req, res);
 });
 
-router.get('/create_order/:id', function (req, res, next) {
-  controller.showBookingPage(req, res);
-});
+// router.get('/create_order/:id', function (req, res, next) {
+//   controller.showBookingPage(req, res);
+// });
 
-router.get('/booked_ticket/:id', function (req, res, next) {
-  controller.showBookedTicket(req, res);
-});
+router.get('/create_order/:id', verifyAuth, controller.showBookingPage);
+
+router.get('/edit_order/:id', verifyAuth, controller.editOrderPage);
+
+router.get('/booked_ticket/:id', verifyAuth, controller.showBookedTicket);
+
+// router.get('/', verifyAuth, accountController.getProfile);
 
 router.get('/cancel_ticket', function (req, res, next) {
   controller.showCancelTicket(req, res);
