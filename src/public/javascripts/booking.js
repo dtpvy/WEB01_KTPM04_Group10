bf9697b4379d5ceaff5c7f601a270293033c7e12
qmck3 <quangfreshmail123@gmail.com>
2023-01-07 13:41:46 +0700

    update booking handle form

--- src/public/javascripts/booking.js
+++ src/public/javascripts/booking.js
@@ -1,5 +1,5 @@
-import { doc } from('prettier');
-import models from('../../models');
+// import { doc } from('prettier');
+// import models from('../../models');
 
 function chooseSeat() {
   if (event.target.classList.contains('seat-choose')) {
@@ -9,37 +9,38 @@ function chooseSeat() {
   }
 }
 
-document.querySelector('.seat--padding').addEventListener('click',(event)=>{
-  if (event.target.classList.contains('seat-choose')) {
-    event.target.classList.remove('seat-choose');
-  } else {
-    event.target.classList.add('seat-choose');
-  }
-})
+// document.querySelector('.seat--padding').addEventListener('onclick', (event) => {
+//   if (event.target.classList.contains('seat-choose')) {
+//     event.target.classList.remove('seat-choose');
+//   } else {
+//     event.target.classList.add('seat-choose');
+//   }
+// });
 
 // //handle form
-// const form = document.getElementById('booking_form');
-// form.addEventListener('submit,', function (event) {
-
-//   event.preventDefault();
-//   const idOrderSeats = document.getElementById('booking_order_seat');
-//   const userId = document.getElementById('');
-//   const routeId = document.getElementById('');
-//   const paymentMethods = document.querySelectorAll('input[name="payment_method"]');
-//   let selectedMethod;
-//   for (const paymentMethod of paymentMethods) {
-//       if (paymentMethod.checked) {
-//           selectedMethod = paymentMethod.value;
-//           break;
-//       }
-//   }
-//   if (selectedMethod == "booking_momo_method") {
-//     console.log('momo');
-//   }else if(selectedMethod == "booking_directly_method"){
-//     console.log('directly');
-//   }
-//   return false;
-// });
+const form = document.getElementById('booking_form');
+form.addEventListener('submit', function () {
+  event.preventDefault();
+  const idOrderSeats = document.getElementById('booking_order_seat');
+  const userId = document.getElementById('');
+  const routeId = document.getElementById('');
+  const paymentMethods = document.querySelectorAll('input[name="payment_method"]');
+  let selectedMethod;
+  for (const paymentMethod of paymentMethods) {
+    if (paymentMethod.checked) {
+      selectedMethod = paymentMethod.value;
+      break;
+    }
+  }
+  if (selectedMethod == 'booking_momo_method') {
+    console.log('momo');
+  } else if (selectedMethod == 'booking_directly_method') {
+    console.log('directly');
+    console.log(route.id);
+    window.location.href = 'http://www.w3schools.com';
+  }
+  return false;
+});
 
 // function submitHandle(){
 //   event.preventDefault();
